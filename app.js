a40352af176efff2899f9840a274a2354c9347f9
abu-kausar <abukausar33@gmail.com>
2022-12-16 21:59:40 +0600

    updated

---
+++ app.js
@@ -0,0 +1,35 @@
+const searchSong = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
+    
+    //fetching the data
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songs.forEach(song => {
+        console.log(song);
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+        </audio>
+        `;
+        songContainer.appendChild(songDiv);
+    });
+}
+
+const getLyrics = (artist, title) => {
+    console.log(artist, title);
+}
